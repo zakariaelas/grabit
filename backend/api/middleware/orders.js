cbff8067e44f621aaeabeb4355da1e0c6daa8ea4
zakariasaad <zakaria.elas@gmail.com>
2020-06-12 20:57:00 +0100

    add: get order by id route

--- backend/api/middleware/orders.js
+++ backend/api/middleware/orders.js
@@ -1,12 +1,13 @@
 const orderService = require('../../services/orders');
-const ForbiddenError = require('../../errors');
+const { ForbiddenError } = require('../../errors');
 
 const ensureCorrectOrderUser = async (req, res, next) => {
   try {
     const { oid } = req.params;
     const { id: uid } = req.user;
     const order = await orderService.getOrder(oid);
-    if (order.customer === uid || order.driver === uid) return next();
+    if (order.customer.toString() === uid || order.driver.toString() === uid)
+      return next();
     return next(new ForbiddenError());
   } catch (err) {
     next(err);
